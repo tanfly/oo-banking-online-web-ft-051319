c01616e6606cdc47411c10cb851d231f1026af6e
Stella McEwen <mcewen.stella@gmail.com>
2019-05-25 21:20:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,6 +27,7 @@ class Transfer
   def reverse_transfer
     if @status == "complete"
       @sender.balance += @amount 
-      @receiver.balance -= @amount
+      @receiver.balance -= @amount 
+      @status = "reversed"
   
 end
